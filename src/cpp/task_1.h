a62be491af2f0736b9deda82bb664b11e0aac113
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-10-25 22:50:21 +0500

    Update task_1.h

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -1,5 +1,23 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Jamol 
+ * Date: 25.10.2023
+ * Name: Jamol
  */
+
+#include <iostream>
+
+int leapYear() {
+    int lYear;
+
+    // Prompt the user to enter a year
+    std::cout << "Enter a year: ";
+    std::cin >> lYear;
+
+    if ((lYear % 4 == 0 && lYear % 100 != 0) || (lYear % 400 == 0)) {
+        std::cout << lYear << " is a leap year." << std::endl;
+    } else {
+        std::cout << lYear << " is not a leap year." << std::endl;
+    }
+
+    return 0;
+}
